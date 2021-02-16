be5f2c1490f7815d2aed9c4f3449afd3078a4286
VaseRajaKrishna <vaseraja888@gmail.com>
2021-02-16 18:56:20 +0530

    bootstrap removed

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var _crsl = 0;
+
     $(".flip-card-btn-turn-to-back").css("visibility", "visible");
     $(".flip-card-btn-turn-to-front").css("visibility", "visible");
 
@@ -15,6 +17,46 @@ $(document).ready(function() {
             $(".click-flip-card:eq(" + index + ")").toggleClass('do-flip');
         });
 
+    });
+
+    // custom Carousel Interaction
+
+    function cstCrsl() {
+        $(".item").hide();
+        $(".item:eq(0)").fadeIn(500);
+        $(".crs-indtrs li:eq(0)").addClass("active");
+
+        $(".right").click(function() {
+            _crsl++;
+            if (_crsl >= 4) {
+                _crsl = 0;
+            }
+            crsActive();
+        });
+
+        $(".left").click(function() {
+            if (_crsl <= 0) {
+                _crsl = 4;
+            }
+            _crsl--;
+            crsActive();
+        });
+
+
+    }
+
+    $(".crs-indtrs li").each(function(index, item) {
+        $(this).click(function() {
+            _crsl = index;
+            crsActive()
+        })
     })
 
+    function crsActive() {
+        $(".item").hide();
+        $(".crs-indtrs li").removeClass("active")
+        $(".item:eq(" + _crsl + ")").fadeIn(500);
+        $(".crs-indtrs li:eq(" + _crsl + ")").addClass("active");
+    }
+    cstCrsl();
 });
